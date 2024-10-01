5cc85b31bf89dd4f2499e8703efad1b2de6e2f39
Felipe Tadeu <felipetadeu.dev@gmail.com>
2024-10-01 15:55:35 -0300

    voltado ao normal

--- app.py
+++ app.py
@@ -723,4 +723,4 @@ app.register_blueprint(webhook)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=3001)
+    app.run(debug=True, host='0.0.0.0', port=3000)
